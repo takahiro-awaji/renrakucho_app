14f095b1e411bda684c26095c440bf4724f9b8ac
takahiro-awaji <69838495+takahiro-awaji@users.noreply.github.com>
2020-10-26 10:57:29 +0900

    投稿編集、削除機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @post = Post.new
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def new
@@ -17,9 +17,35 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
   private
 
   def post_params
-    params.require(:post).permit(:content).merge(user_id: current_user.id)
+    params.require(:post).permit(:title, :content, :image).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'posts/index'
   devise_for :users
   root "posts#index"
-  resources :posts , only: [:index, :new, :create]
+  resources :posts
 end
