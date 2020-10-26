5fb9dda042d7a769bfb5673e3039888ad40ffe2d
takahiro-awaji <69838495+takahiro-awaji@users.noreply.github.com>
2020-10-26 19:15:25 +0900

    検索結果表示順の修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,14 +2,14 @@ class Post < ApplicationRecord
   belongs_to :user, optional: true
   has_many :likes, dependent: :destroy
   has_one_attached :image
-  has_many :post_tag_relations
+  has_many :post_tag_relations, dependent: :destroy
   has_many :tags, through: :post_tag_relations
 
   def self.search(search)
     if search != ""
-      Post.where('content LIKE(?) or title LIKE(?)', "%#{search}%", "%#{search}%")
+      Post.where('content LIKE(?) or title LIKE(?)', "%#{search}%", "%#{search}%").order(created_at: :desc)
     else
-      Post.all
+      Post.all.order(created_at: :desc)
     end
   end
 
